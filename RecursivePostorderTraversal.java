ce3e474c45bafe1fb2cd6bf7c9d3a32713d9096e
Prashant1099 <patelprashant356@gmail.com>
2022-01-11 12:11:17 +0530

    Added Postorder traversal of any Binary Tree question

---
+++ RecursivePostorderTraversal.java
@@ -0,0 +1,40 @@
+// Question Link: https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class RecursivePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        
+        traversal(root, ans);
+        
+        return ans;
+    }
+    
+    public void traversal(TreeNode root, List<Integer> ans){
+        if(root == null) return;
+        
+        traversal(root.left, ans);
+        traversal(root.right, ans);
+        ans.add(root.val);
+    }
+}
+
+
