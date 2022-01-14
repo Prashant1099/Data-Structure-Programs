62642516ba8e932f369b095f008a5d1df1476c29
Prashant1099 <patelprashant356@gmail.com>
2022-01-14 14:52:54 +0530

    Added Check Symmetric Tree question

---
+++ Tree/CheckSymmetricTree.java
@@ -0,0 +1,35 @@
+// Question link: https://leetcode.com/problems/symmetric-tree/
+
+/*
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class CheckSymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return checkSymmetric(root.left, root.right);
+    }
+    
+    public boolean checkSymmetric(TreeNode left, TreeNode right){
+        if(left == null || right == null) return (left == right);
+        
+        if(left.val != right.val) return false;
+        
+        return checkSymmetric(left.left, right.right) && checkSymmetric(left.right, right.left);
+    }
+}
