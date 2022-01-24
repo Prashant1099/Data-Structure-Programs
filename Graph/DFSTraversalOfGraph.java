4f3582d83cb03b59ced05a4be68b403fba3ca5f3
Prashant1099 <patelprashant356@gmail.com>
2022-01-24 15:25:21 +0530

    Added Depth first search of Graph question

---
+++ Graph/DFSTraversalOfGraph.java
@@ -0,0 +1,61 @@
+// Question Link: https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1/#
+
+/* Given a connected undirected graph. Perform a Depth First Traversal of the graph.
+Note: Use recursive approach to find the DFS traversal of the graph starting from the 0th vertex from left to right according to the graph..
+*/
+
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class DFSTraversalOfGraph {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean []visit = new boolean[V+1];
+        
+        dfs(0, visit, adj, ans);
+        
+        return ans;
+    }
+    
+    public void dfs(int node, boolean []visit, ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ans){
+        ans.add(node);
+        visit[node] = true;
+        
+        for(Integer it: adj.get(node)){
+            if(visit[it] == false) dfs(it, visit, adj, ans);
+        }
+    }
+}
